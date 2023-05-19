5725373ff8d8a6ecf6644a3e2f8d89dcd1dfb711
gabriela-giron <krodriguezg4@miumg.edu.gt>
2023-05-18 20:19:40 -0600

    first sketches
    img and forms

---
+++ paginas/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Los Jardines.</title>
+</head>
+<body>
+    <div class="nav">
+        <h3 class="titulo-nav">Los Jardines.</h3>
+        <nav class="navegacion">
+            <ul class="opciones">
+                <li><a href="visitas.html">Solicitar visita</a></li>
+                <li><a href="#">Residentes</a></li>
+            </ul>
+        </nav>
+    </div>
+    <hr>
+    <div class="portada">
+        <h5>Residenciales</h5>
+        <h1 class="titulo">Los Jardines.</h1>
+        <img src="../img/FACHADA1.png" alt="">
+    </div>
+    <div class="info">
+        <section class="fachada-1">
+            <img src="../img/FACHADA3.png" alt="">
+            <ul>
+                <h5>donde tu comodidad es nuestra</h5>
+                <h1 class="titulo2">Prioridad</h1>
+            </ul>
+        </section>
+
+        <section class="fachada-2">
+            <img class="patio" src="../img/PATIO3.png" alt="">
+            <img class="ventana" src="../img/VENTANA.png" alt="">
+        </section>
+        
+    </div>
+</body>
+</html>
