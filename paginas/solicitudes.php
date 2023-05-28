378ee7a7c07f87a5f2ded1d954a7372fe86cff40
mblunaa <131736939+mblunaa@users.noreply.github.com>
2023-05-28 13:01:12 -0600

    Tabla donde se visualicen las solicitudes
    La tabla muestra las solicitudes pendientes, denegadas y aprobadas (debe estar enlazada al usuario)

---
+++ paginas/solicitudes.php
@@ -0,0 +1,48 @@
+<?php
+include("conexion.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Solicitudes</title>
+</head>
+
+<body>
+    <section class="fachada-1">
+        <h1 align="center">Solicitudes de ingreso</h1>
+        <p align="center">Te mostramos las solicitudes de ingreso que has recibido últimamente.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <table style="border-spacing: 50px;" align="center">
+            <tr>
+                <th align="center">Pendientes</th>
+                <th align="center">Aceptados</th>
+                <th align="center">Denegados</th>
+            </tr>
+            
+            <?php
+                //$pendiente=
+               // $aprobada=
+                //$denegada=
+            ?>
+        </table>
+
+
+    </section>
+</body>
+
+</html>
