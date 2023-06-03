fed25340082caf64084aba1e7388d0af5e5a0d20
gabriela-giron <79176863+gabriela-giron@users.noreply.github.com>
2023-06-03 09:36:34 -0600

    Update bienvenido.php

--- paginas/bienvenido.php
+++ paginas/bienvenido.php
@@ -129,7 +129,7 @@ $numeroCasa = $fila['NUMERO_CASA'];
 
             for ($i = 0; $i < $maxRows; $i++) {
                 echo '<tr>';
-                echo '<td><button onclick="document.getElementById('id01').style.display='block'" class="user">
+                echo '<td><button onclick="document.getElementById(\'id01\').style.display=\'block\'" class="user">
                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
                     </button>' . (isset($pendientes[$i]) ? $pendientes[$i] : '') . '</td>';
                 echo '<td>' . (isset($aceptados[$i]) ? $aceptados[$i] : '') . '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg></td>';
