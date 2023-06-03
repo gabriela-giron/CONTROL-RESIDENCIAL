a7d843211debe006ee2d779c4a8eb6dc8531f461
gabriela-giron <79176863+gabriela-giron@users.noreply.github.com>
2023-06-03 07:08:57 -0600

    Update bienvenido.php
    se agrego ventana modal

--- paginas/bienvenido.php
+++ paginas/bienvenido.php
@@ -64,6 +64,35 @@ $numeroCasa = $fila['NUMERO_CASA'];
         <br>
         <br>
         <br>
+         <div id="id01" class="w3-modal">
+		<span onclick="document.getElementById('id01').style.display='none'" class="w3-closebtn w3-hover-red w3-container w3-padding-hor-8 w3-display-topright">&times;</span>
+		<div class="w3-modal-content w3-card-8 w3-animate-top" style="max-width:600px">
+
+			<section class="card-solicitud">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-left"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
+                <h2>Solicitud.</h2>
+                        <p>Nombre Completo del Visitante: <?php echo $nombre_visita . ' ' . $apellido; ?></p>
+                        <p>DPI: <?php echo $dpi_visita; ?></p>
+                        <p>Descripcion del Vehículo: <?php echo $descripcion_visita; ?></p>
+                        <p>Telefono: <?php echo $telefono_visita; ?></p>
+                        <p>Fecha de ingreso: <?php echo $fecha_visita; ?></p>
+                        <p>Motivo: <?php echo $motivo_visita; ?></p>
+                <article>
+                    <p>¿Qué deseas realizar con esta solicitud?</p>
+                    
+                    <a href="actualizar.php?codigo=<?php echo $codigo; ?>&id_detalle_visita=<?php echo $id_detalle_visita; ?> ">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-check"><polyline points="20 6 9 17 4 12"></polyline></svg>
+                    </a>
+                    
+                    <a href="denegar.php?codigo=<?php echo $codigo; ?>&id_detalle_visita=<?php echo $id_detalle_visita; ?> ">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+                    </a>
+                        
+                
+                </article>
+			</section>
+
+		</div>
         <table style="border-spacing: 50px;" align="center">
             <tr>
                 <th align="center">Pendientes</th>
@@ -100,9 +129,11 @@ $numeroCasa = $fila['NUMERO_CASA'];
 
             for ($i = 0; $i < $maxRows; $i++) {
                 echo '<tr>';
-                echo '<td>' . (isset($pendientes[$i]) ? $pendientes[$i] : '') . '</td>';
-                echo '<td>' . (isset($aceptados[$i]) ? $aceptados[$i] : '') . '</td>';
-                echo '<td>' . (isset($denegados[$i]) ? $denegados[$i] : '') . '</td>';
+                echo '<td><button onclick="document.getElementById('id01').style.display='block'" class="user">
+                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
+                    </button>' . (isset($pendientes[$i]) ? $pendientes[$i] : '') . '</td>';
+                echo '<td>' . (isset($aceptados[$i]) ? $aceptados[$i] : '') . '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg></td>';
+                echo '<td>' . (isset($denegados[$i]) ? $denegados[$i] : '') . '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg></td>';
                 echo '</tr>';
             }
             ?>
