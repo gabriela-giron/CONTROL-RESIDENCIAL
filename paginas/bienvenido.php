9158db68d8e95e9c2aad49c25d3b0f75d809b41d
mblunaa <131736939+mblunaa@users.noreply.github.com>
2023-06-03 09:50:47 -0600

    x

--- paginas/bienvenido.php
+++ paginas/bienvenido.php
@@ -17,7 +17,7 @@ $fila = mysqli_fetch_assoc($resultado);
 
 // Mostrar los datos del residente
 $nombre = $fila['NOMBRE'];
-$apellido = $fila['APELLIDO'];
+$apellido = $fila ['APELLIDO'];
 $codigo = $fila['ID_RESIDENTE'];
 $telefono = $fila['TELEFONO'];
 $correo = $fila['CORREO'];
@@ -27,9 +27,12 @@ $numeroCasa = $fila['NUMERO_CASA'];
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
+ <meta charset="UTF-8">
+    <script src="https://kit.fontawesome.com/67fe543a2c.js" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	 <script src="https://www.w3schools.com/lib/w3.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
@@ -41,152 +44,83 @@ $numeroCasa = $fila['NUMERO_CASA'];
     <link rel="stylesheet" href="css/styles.css">
     <title>Bienvenido</title>
 </head>
-
 <body>
 
-    <div class="datos">
-        <h1>Bienvenido,
-            <?php echo $nombre; ?>!
-        </h1>
-        <p>codigo:
-            <?php echo $codigo; ?>
-        </p>
-        <p>Nombre:
-            <?php echo $nombre . ' ' . $apellido; ?>
-        </p>
-        <p>Apellido:
-            <?php echo $apellido; ?>
-        </p>
-        <p>Número de Casa:
-            <?php echo $numeroCasa; ?>
-        </p>
-        <p>Correo Electronico:
-            <?php echo $correo; ?>
-        </p>
-        <p>Teléfono:
-            <?php echo $telefono; ?>
-        </p>
-    </div>
+<div class="datos">
+    <h1>Bienvenido, <?php echo $nombre; ?>!</h1>
+    <p>codigo: <?php echo $codigo; ?></p>
+    <p>Nombre: <?php echo $nombre . ' ' . $apellido; ?></p>
+    <p>Apellido: <?php echo $apellido; ?></p>
+    <p>Número de Casa: <?php echo $numeroCasa; ?></p>
+    <p>Correo Electronico: <?php echo $correo; ?></p>
+    <p>Teléfono: <?php echo $telefono; ?></p>
+</div>
 
 
     <br>
     <br>
     <br>
 
-    <section class="fachada-1">
+     <section class="fachada-1">
+        <h1 align="center">Solicitudes de ingreso</h1>
+        <p align="center">Te mostramos las solicitudes de ingreso que has recibido últimamente.</p>
+        <br>
+    <br>
+    <br>
+
+     <section class="fachada-1">
         <h1 align="center">Solicitudes de ingreso</h1>
         <p align="center">Te mostramos las solicitudes de ingreso que has recibido últimamente.</p>
         <br>
         <br>
         <br>
         <br>
-        <div id="id01" class="w3-modal">
-            <span onclick="document.getElementById('id01').style.display='none'"
-                class="w3-closebtn w3-hover-red w3-container w3-padding-hor-8 w3-display-topright">&times;</span>
-            <div class="w3-modal-content w3-card-8 w3-animate-top" style="max-width:600px">
+        <table style="border-spacing: 50px;" align="center">
+            <tr>
+                <th align="center">Pendientes</th>
+                <th align="center">Aceptados</th>
+                <th align="center">Denegados</th>
+            </tr>
 
-                <section class="card-solicitud">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                        class="feather feather-arrow-left">
-                        <line x1="19" y1="12" x2="5" y2="12"></line>
-                        <polyline points="12 19 5 12 12 5"></polyline>
-                    </svg>
-                    <h2>Solicitud.</h2>
-                    <p>Nombre Completo del Visitante:
-                        <?php echo $nombre_visita . ' ' . $apellido; ?>
-                    </p>
-                    <p>DPI:
-                        <?php echo $dpi_visita; ?>
-                    </p>
-                    <p>Descripcion del Vehículo:
-                        <?php echo $descripcion_visita; ?>
-                    </p>
-                    <p>Telefono:
-                        <?php echo $telefono_visita; ?>
-                    </p>
-                    <p>Fecha de ingreso:
-                        <?php echo $fecha_visita; ?>
-                    </p>
-                    <p>Motivo:
-                        <?php echo $motivo_visita; ?>
-                    </p>
-                    <article>
-                        <p>¿Qué deseas realizar con esta solicitud?</p>
+            <?php
+            //codigo para mostrar las solicitudes en la tabla
+            $pendiente = "SELECT NOMBRE_COMPLETO FROM VISTA_SOLICITUD WHERE ID_STATUS = 0 AND ID_RESIDENTE=".$codigo;
+            $aprobada = "SELECT NOMBRE_COMPLETO FROM VISTA_SOLICITUD WHERE ID_STATUS = 1 AND ID_RESIDENTE=".$codigo;
+            $denegada = "SELECT NOMBRE_COMPLETO FROM VISTA_SOLICITUD WHERE ID_STATUS = 2 AND ID_RESIDENTE=".$codigo;
+            $ejecutarp = mysqli_query($conn, $pendiente);
+            $ejecutara = mysqli_query($conn, $aprobada);
+            $ejecutard = mysqli_query($conn, $denegada);
 
-                        <a
-                            href="actualizar.php?codigo=<?php echo $codigo; ?>&id_detalle_visita=<?php echo $id_detalle_visita; ?> ">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-check">
-                                <polyline points="20 6 9 17 4 12"></polyline>
-                            </svg>
-                        </a>
+            $pendientes = array();
+            $aceptados = array();
+            $denegados = array();
 
-                        <a
-                            href="denegar.php?codigo=<?php echo $codigo; ?>&id_detalle_visita=<?php echo $id_detalle_visita; ?> ">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-x">
-                                <line x1="18" y1="6" x2="6" y2="18"></line>
-                                <line x1="6" y1="6" x2="18" y2="18"></line>
-                            </svg>
-                        </a>
+            while ($busqueda = mysqli_fetch_array($ejecutarp)) {
+                $pendientes[] = $busqueda[0];
+            }
 
+            while ($busqueda2 = mysqli_fetch_array($ejecutara)) {
+                $aceptados[] = $busqueda2[0];
+            }
 
-                    </article>
-                </section>
+            while ($busqueda3 = mysqli_fetch_array($ejecutard)) {
+                $denegados[] = $busqueda3[0];
+            }
 
-            </div>
-            <table style="border-spacing: 50px;" align="center">
-                <tr>
-                    <th align="center">Pendientes</th>
-                    <th align="center">Aceptados</th>
-                    <th align="center">Denegados</th>
-                </tr>
+            $maxRows = max(count($pendientes), count($aceptados), count($denegados));
 
-                <?php
-                //codigo para mostrar las solicitudes en la tabla
-                $pendiente = "SELECT NOMBRE_COMPLETO FROM VISTA_SOLICITUD WHERE ID_STATUS = 0 AND ID_RESIDENTE=" . $codigo;
-                $aprobada = "SELECT NOMBRE_COMPLETO FROM VISTA_SOLICITUD WHERE ID_STATUS = 1 AND ID_RESIDENTE=" . $codigo;
-                $denegada = "SELECT NOMBRE_COMPLETO FROM VISTA_SOLICITUD WHERE ID_STATUS = 2 AND ID_RESIDENTE=" . $codigo;
-                $ejecutarp = mysqli_query($conn, $pendiente);
-                $ejecutara = mysqli_query($conn, $aprobada);
-                $ejecutard = mysqli_query($conn, $denegada);
-
-                $pendientes = array();
-                $aceptados = array();
-                $denegados = array();
-
-                while ($busqueda = mysqli_fetch_array($ejecutarp)) {
-                    $pendientes[] = $busqueda[0];
-                }
-
-                while ($busqueda2 = mysqli_fetch_array($ejecutara)) {
-                    $aceptados[] = $busqueda2[0];
-                }
-
-                while ($busqueda3 = mysqli_fetch_array($ejecutard)) {
-                    $denegados[] = $busqueda3[0];
-                }
-
-                $maxRows = max(count($pendientes), count($aceptados), count($denegados));
-
-                for ($i = 0; $i < $maxRows; $i++) {
-                    echo '<tr>';
-                    echo '<td><button onclick="document.getElementById(\'id01\').style.display=\'block\'" class="user">
-                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
-                    </button>' . (isset($pendientes[$i]) ? $pendientes[$i] : '') . '</td>';
-                    echo '<td>' . (isset($aceptados[$i]) ? $aceptados[$i] : '') . '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg></td>';
-                    echo '<td>' . (isset($denegados[$i]) ? $denegados[$i] : '') . '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg></td>';
-                    echo '</tr>';
-                }
-                ?>
-            </table>
+            for ($i = 0; $i < $maxRows; $i++) {
+                echo '<tr>';
+                echo '<td>' . (isset($pendientes[$i]) ? $pendientes[$i] : '') . '</td>';
+                echo '<td>' . (isset($aceptados[$i]) ? $aceptados[$i] : '') . '</td>';
+                echo '<td>' . (isset($denegados[$i]) ? $denegados[$i] : '') . '</td>';
+                echo '</tr>';
+            }
+            ?>
+        </table>
 
 
     </section>
 
 </body>
-
 </html>
